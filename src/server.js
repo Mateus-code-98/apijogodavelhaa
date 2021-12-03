ecb3f883e547ccbe227046fd12e129aa1dd4d2d9
mateus-code-98 <teu-9@hotmail.com>
2021-12-03 01:30:53 -0300

    Implementando lógica do vencedor da partida

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ require('express-async-errors');
 require('./socket')
 
 const { serverHttp } = require('./http');
+const { hasWinnerService } = require('./services/friendshipsService');
 
 const port = process.env.PORT ? process.env.PORT : 3000;
+
+console.log(hasWinnerService({game:[
+    [1,0,0],
+    [1,-1,0],
+    [-1,0,1]
+]}))
 
 serverHttp.listen(port, '192.168.0.103', () => {
     console.log(`Server is running on PORT ${port}`)
--- src/services/friendshipsService.js
+++ src/services/friendshipsService.js
@@ -59,6 +59,13 @@ const newMoveService = async ({ friendshipId, x, y }) => {
     const gameArray = JSON.parse(friendship.game)
     gameArray[y][x] = newMoveType
 
+    const hasWinner = hasWinnerService({ game: gameArray })
+    if (hasWinner.status){
+        friendship.status = 'finished'
+        if(hasWinner.type === 'X')friendship.victories_x += 1
+        else friendship.victories_o += 1
+    }
+
     friendship.turn = newTurn
     friendship.game = JSON.stringify(gameArray)
 
@@ -67,4 +74,40 @@ const newMoveService = async ({ friendshipId, x, y }) => {
     return friendship
 }
 
-module.exports = { createFriendshipService, getFriendsService, getFriendshipService, newMoveService }
+const hasWinnerService = ({ game }) => {
+
+    for (let i = 0; i < 3; i++) {
+        const line = []
+        const column = []
+        for (let j = 0; j < 3; j++) {
+            line.push(game[i][j])
+            column.push(game[j][i])
+        }
+        const resu_line = thisLineIsCompletedService(line)
+        const resu_column = thisLineIsCompletedService(column)
+        if (resu_line.status) return resu_line
+        if (resu_column.status) return resu_column
+    }
+
+    const case_especific_1 = thisLineIsCompletedService([game[0][0], game[1][1], game[2][2]])
+    const case_especific_2 = thisLineIsCompletedService([game[0][2], game[1][1], game[2][0]])
+    if (case_especific_1.status) return case_especific_1
+    if (case_especific_2.status) return case_especific_2
+
+    return { status: false }
+}
+
+const thisLineIsCompletedService = (line) => {
+    let contX = 0;
+    let contO = 0;
+    line.forEach((item) => {
+        if (item === 1) contO++
+        else if (item === -1) contX++
+    })
+
+    if (contO === 3) return { status: true, type: 'O' }
+    if (contX === 3) return { status: true, type: 'X' }
+    return { status: false }
+}
+
+module.exports = { createFriendshipService, hasWinnerService, getFriendsService, getFriendshipService, newMoveService }
